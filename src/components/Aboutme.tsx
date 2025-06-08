4d54e2a60f1d111ffb23e1c4f1b0b54a61ce6b27
Adrian Enis <adrianenis2000@gmail.com>
2025-06-08 12:28:33 -0400

    commit

--- src/components/Aboutme.tsx
+++ src/components/Aboutme.tsx
@@ -1,18 +1,22 @@
-
-
 export const Aboutme = () => {
   return (
     <article className=" text-white max-w-[1200px] w-[90%] md:w-[75%] lg:w-[50%] m-auto mt-10 p-5 rounded-2xl">
-    <h2 className="text-center font-extrabold text-3xl md:text-4xl text-yellow-500 mb-5">About Me</h2>
-    <p className="text-justify text-lg">
-      Hola que tal!, me llamo adrian y soy desarrollador Front-end, cuento con 5 años de formacion academica y especializada.
-      Empece a programar en c++, gracias a ello
-      me percate que mi talento estaba en otro lado, y fue alli donde conoci el desarrollo front end.
-      Empece a maquetar paginas web en Html y css... Con el tiempo me di cuenta que necesitaba mas dinamismo y
-      generar cierta interaccion entre el usuario y la pagina, por lo que me adentre en el lenguaje de javascript,
-      el cual me ayudo a entender mejor como funcionaba este maravilloso ecosistema en el cual hoy dia desarrollo.
-     
-    </p>
-  </article>
-  )
-}
+      <h2 className="text-center font-extrabold text-3xl md:text-4xl text-yellow-500 mb-5">
+        About Me
+      </h2>
+      <p className="text-justify text-lg">
+        Hi, I'm Adrian, a Front-End Developer with over four years of experience
+        in creating and optimizing digital platforms for e-commerce businesses.
+        My background combines academic knowledge with continuous learning in
+        key web development technologies. I specialize in designing modern,
+        efficient, and accessible interfaces using tools like React, TypeScript,
+        and TailwindCSS. I help companies achieve their goals by implementing
+        solutions that enhance user experience and optimize website performance.
+        From API integrations to load and performance optimization, my focus is
+        on boosting platform visibility on the web and ensuring smooth
+        navigation for users. I'm always seeking new challenges that allow me to
+        keep growing and bring value to innovative digital projects.
+      </p>
+    </article>
+  );
+};
